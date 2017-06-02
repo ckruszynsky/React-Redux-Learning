660cf9002901dd25dc1a848e95e8094ae52ab256
Chad Kruszynsky <ckruszynsky@gmail.com>
2017-06-02 07:03:47 -0500

    Added Search debounce

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import SearchBar from "./search_bar";
 import VideoList from "./video_list";
 import VideoDetail from "./video_detail";
 import YTSearch from "youtube-api-search";
+import _ from 'lodash';
 
 const API_KEY = "AIzaSyCBy17oDE2r9Jb2HFBYA8xuOR9uH1vqChE";
 
@@ -26,9 +27,10 @@ class App extends Component {
     });
   }
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar  onSearchTermChange = { term => this.videoSearch(term) } />
+        <SearchBar  onSearchTermChange = {videoSearch } />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
